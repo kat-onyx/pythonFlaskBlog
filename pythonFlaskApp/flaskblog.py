956207b32f54fedb72ebf6bfa1708c6b8417213c
kat <sarah.kathryn.peters@gmail.com>
2019-05-01 15:44:07 -0700

    Fix typo in app config for db

--- pythonFlaskApp/flaskblog.py
+++ pythonFlaskApp/flaskblog.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATSBASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
 
