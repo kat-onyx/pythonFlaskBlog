43f8bb6e92be8c4b84c95450be85709ac2abcf99
kat <sarah.kathryn.peters@gmail.com>
2019-05-01 15:43:15 -0700

    Add User model

--- pythonFlaskApp/flaskblog.py
+++ pythonFlaskApp/flaskblog.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATSBASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.column(db.String(20), unique=True, nullable=False)
+    username = db.column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        # print out
+        return f"User('{self.username}', '{self.email}')"
+
+
 @app.route("/")
+@app.route("/home")
 def hello():
     return render_template('home.html')
 
