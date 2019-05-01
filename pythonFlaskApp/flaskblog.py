870fdf92e66e533727090d8c2ccf91cc258d0601
kat <sarah.kathryn.peters@gmail.com>
2019-05-01 14:16:10 -0700

    Create templates folder and home template to render in app page

--- pythonFlaskApp/flaskblog.py
+++ pythonFlaskApp/flaskblog.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html')
 
 @app.route("/about")
 def about():
