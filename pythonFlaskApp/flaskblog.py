0ee23bf132b87fd50d61a88ecc940938fd3f80db
kat <sarah.kathryn.peters@gmail.com>
2019-05-02 18:13:22 -0700

    Fixing issue

--- pythonFlaskApp/flaskblog.py
+++ pythonFlaskApp/flaskblog.py
@@ -1,21 +1,23 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+
+
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
 db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.column(db.String(20), unique=True, nullable=False)
-    username = db.column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False,
+                           default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
 
     def __repr__(self):
-        # print out
-        return f"User('{self.username}', '{self.email}')"
-
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 @app.route("/")
 @app.route("/home")
@@ -25,3 +27,7 @@ def hello():
 @app.route("/about")
 def about():
     return "<h1>About Page</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
